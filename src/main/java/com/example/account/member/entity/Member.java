1ff09c618b8b4b1e6dbc3257f3ed7d94de514e37
junhaa <2171326@hansung.ac.kr>
2024-05-16 02:24:25 +0900

    :sparkles: FEAT. Member Entity 구현

---
+++ src/main/java/com/example/account/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.example.account.member.entity;
+
+import com.example.account.util.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "members")
+public class Member extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "user_id", nullable = false)
+	private Long userId;
+
+	@Column(name = "password", nullable = false)
+	private Long password;
+
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	@Column(name = "phone", nullable = false)
+	private String phone;
+
+}
