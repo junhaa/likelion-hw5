5380005a1a70ff1c67c497ac92b9b3de5371b9fd
junhaa <2171326@hansung.ac.kr>
2024-05-16 03:04:11 +0900

    :art: CHORE. 로그인 관련 예외 에러코드 추가

--- src/main/java/com/example/account/util/enums/ErrorStatus.java
+++ src/main/java/com/example/account/util/enums/ErrorStatus.java
@@ -7,7 +7,13 @@ public enum ErrorStatus implements BaseCode {
 	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
 	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
 	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+	// member
+
+	_MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4001", "존재하지 않는 회원입니다."),
+	_PASSWORD_INVAILD(HttpStatus.BAD_REQUEST, "MEMBER4002", "비밀번호가 일치하지 않습니다.");
+
 
 	private final HttpStatus httpStatus;
 	private final String code;
